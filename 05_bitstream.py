1290de23695304e885ec7822bd5b1daf97c95b1a
Dua Baig <dbaig50@stuy.edu>
2024-09-19 10:12:45 -0400

    finished code

--- 05_bitstream.py
+++ 05_bitstream.py
@@ -1,22 +1,29 @@
-#<Frist> <Lsat>
+#<Dua> <Baig>
 #<TNPG>
 #SoftDev
-#K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
-#<yyyy>-<mm>-<dd>
-#time spent: <elapsed time in hours, rounded to nearest tenth>
-#<TNPG>
-#SoftDev
-#K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
-#<yyyy>-<mm>-<dd>
-#time spent: <elapsed time in hours, rounded to nearest tenth>
-
+#K<nn> -- <Bitstream/Bitstream/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+#<2024>-<09>-<17>
+#time spent: <.7 hours>
+import random
 krewes = open("krewes.txt", "r")
 info = krewes.read();
 info = info.split("@@@")
+info = info[:-1]
 
-krewesDict = {}
-period = []
-for i in info:
-    period = i.split("$$$")
-    print(period)
+DictList = []
+
+def makeDictList(InfoList):
+    for i in InfoList:
+        periodInfo = i.split("$$$")
+        DictList.append (dict(period = periodInfo[0], devo = periodInfo[1], ducky = periodInfo[2]))
+
+
+makeDictList(info)
+print(DictList)
     
+def ChooseDevo(ListDict):
+    devo = ListDict[random.randint(0, len(ListDict)-1)]
+    devo = devo["devo"]
+    return devo;
+
+print(ChooseDevo(DictList));

ea5d7a4305f1f9e03fb6620bb4b6d0e24dfc14ef
Dua Baig <dbaig50@stuy.edu>
2024-10-09 10:15:54 -0400

    forgot to uncomment out a method

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     # print("\n\n\n")
     # print("***DIAG: this Flask obj ***")

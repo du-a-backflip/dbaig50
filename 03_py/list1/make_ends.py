7068e0f80753d3285c01f270885d156ed40bf6f3
Dua Baig <dbaig50@stuy.edu>
2024-10-16 22:03:18 -0400

    added some list

---
+++ 03_py/list1/make_ends.py
@@ -0,0 +1,5 @@
+def make_ends(nums):
+  num = []
+  num.append(nums[0])
+  num.append(nums[len(nums)-1])
+  return num
---
+++ 03_py/list1/middle_way.py
@@ -0,0 +1,5 @@
+def middle_way(a, b):
+  nums = []
+  nums.append(a[1])
+  nums.append(b[1])
+  return nums
